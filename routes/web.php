0e33615c8ca77da4c668fabf51870df896445df4
Nemo.luong <nemo.luong@innovatube.com>
2018-01-15 17:16:19 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('user','UserController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
